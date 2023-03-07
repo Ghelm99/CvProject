1e0d7dc85156a717ebefe2f90e54c97292f4d8dd
Andrea Galmuzzi <android.bipede@gmail.com>
2023-03-07 15:25:02 +0100

    A working version

--- main.py
+++ main.py
@@ -2,17 +2,15 @@ import cv2 as cv
 import numpy as np
 import imutils
 import easyocr
-import matplotlib
-from matplotlib import pyplot as plt
 
-original = cv.imread('images/car4.jpg')
+original = cv.imread('images/car5.jpg')
 # cv.imshow('Original', original)
 
 gray = cv.cvtColor(original, cv.COLOR_BGR2GRAY)
 # cv.imshow('Gray', gray)
 
-filtered = cv.bilateralFilter(gray, 11, 17, 17)
-# cv.imshow('Filtered', filtered)
+filtered = cv.bilateralFilter(gray, 9, 75, 75)
+ # cv.imshow('Filtered', filtered)
 
 edged = cv.Canny(filtered, 30, 200)
 # cv.imshow('Edged', edged)
@@ -33,19 +31,18 @@ if location is not None:
     mask = np.zeros(gray.shape, np.uint8)
     plate = cv.drawContours(mask, [location], 0,255, -1)
     plate = cv.bitwise_and(original, original, mask=mask)
-    # cv.imshow('Plate', plate)
-    cv.waitKey(0)
 else:
     print("Failed to find plate location.")
 
 (x,y) = np.where(mask==255)
-(x1,y1) = (np.min(x), np.min(y))
-(x2,y2) = (np.max(x), np.max(y))
-shrinked_plate = gray[x1:x2+1, y1:y2+1]
-cv.imshow('Plate', shrinked_plate)
+(x1, y1) = (np.min(x), np.min(y))
+(x2, y2) = (np.max(x), np.max(y))
+cropped_image = gray[x1:x2+1, y1:y2+1]
+
+cv.imshow('Cropped', cropped_image)
 
 reader = easyocr.Reader(['en'])
-result = reader.readtext(shrinked_plate)
+result = reader.readtext(cropped_image)
 print(result)
 
 cv.waitKey(0)
