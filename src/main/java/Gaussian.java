93409b7e3c46eaaa2ecf3925562046c58c439e75
Andrea Galmuzzi <android.bipede@gmail.com>
2023-03-10 07:24:54 +0100

    Fixing comments

--- src/main/java/Gaussian.java
+++ src/main/java/Gaussian.java
@@ -11,15 +11,15 @@ public class Gaussian {
 
   public static void main(String[] args) throws IOException {
 
-    /* Reading input image and creating output image */
+    /* Read input image and create output image */
     BufferedImage input = ImageIO.read(new File("images/input.jpg"));
     BufferedImage output = new BufferedImage(input.getWidth(), input.getHeight(), BufferedImage.TYPE_INT_RGB);
 
-    /* Setting kernel size and standard deviation for Gaussian blur */
+    /* Set kernel size and standard deviation for Gaussian blur */
     int kernelSize = 5;
     double sigma = 1.5;
 
-    /* Creating and initializing the Gaussian kernel */
+    /* Create and initializing the Gaussian kernel */
     double[][] kernel = new double[kernelSize][kernelSize];
     double mean = kernelSize / 2;
     double sum = 0.0;
@@ -30,7 +30,7 @@ public class Gaussian {
         }
     }
 
-    /* Normalizing the kernel */
+    /* Normalize the kernel */
     for (int i = 0; i < kernelSize; i++) {
         for (int j = 0; j < kernelSize; j++) {
             kernel[i][j] /= sum;
@@ -47,14 +47,14 @@ public class Gaussian {
             /* Convolve the kernel around the pixel */
             for (int ki = 0; ki < kernelSize; ki++) {
                 for (int kj = 0; kj < kernelSize; kj++) {
-                    /* Computing index of the pixel */
+                    /* Compute index of the pixel */
                     int pi = i - kernelSize / 2 + ki;
                     int pj = j - kernelSize / 2 + kj;
                     /* Check if pixel is inside of the image */
                     if (pi >= 0 && pi < input.getWidth() && pj >= 0 && pj < input.getHeight()) {
                         /* Get the color of pixel at the current index */
                         Color color = new Color(input.getRGB(pi, pj));
-                        /* Applying the kernel to the color values */
+                        /* Apply the kernel to the color values */
                         red += color.getRed() * kernel[ki][kj];
                         green += color.getGreen() * kernel[ki][kj];
                         blue += color.getBlue() * kernel[ki][kj];
@@ -62,7 +62,7 @@ public class Gaussian {
                 }
             }
 
-            /* Rounding color values to integers and clamping them to 0-255 range */
+            /* Round color values to integers and clamping them to 0-255 range */
             int filteredRed = (int) Math.round(red);
             int filteredGreen = (int) Math.round(green);
             int filteredBlue = (int) Math.round(blue);
